23ac9da7d7530bc8ca94d4b2e626a3e78bbb402c
Devendra Janyani <devjanyani.work@gmail.com>
2026-03-04 20:06:56 -0700

    Updated Main.java

--- graph-parser/src/main/java/edu/asu/cse464/Main.java
+++ graph-parser/src/main/java/edu/asu/cse464/Main.java
@@ -3,12 +3,18 @@ package edu.asu.cse464;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Graph graph = DotParser.parseGraph("input/sample.dot");
-        System.out.println(graph);
+        String inputDot = "input/sample.dot";
 
-        String outFile = "output.png";
-        graph.writePng(outFile);
+        Graph g = DotParser.parseGraph(inputDot);
 
-        System.out.println("Wrote PNG to: " + outFile);
+        System.out.print(g.toString());
+
+        g.outputDOTGraph("output.dot");
+        g.outputGraph("output.txt");
+        g.outputGraphics("output.png", "png");
+
+        System.out.println("Wrote DOT to: output.dot");
+        System.out.println("Wrote graph text to: output.txt");
+        System.out.println("Wrote PNG to: output.png");
     }
 }

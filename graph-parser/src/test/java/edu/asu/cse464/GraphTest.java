15d7dd4c42da9be918ffbb65c2cb45718ece09ca
Devendra Janyani <devjanyani.work@gmail.com>
2026-03-04 18:51:00 -0700

    Add unit tests for Graph functionality

---
+++ graph-parser/src/test/java/edu/asu/cse464/GraphTest.java
@@ -0,0 +1,38 @@
+package edu.asu.cse464;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GraphTest {
+
+    @Test
+    public void testAddNode() {
+        Graph g = new Graph();
+
+        g.addNode("A");
+        g.addNode("B");
+
+        assertEquals(2, g.getNodeCount());
+    }
+
+    @Test
+    public void testAddEdge() {
+        Graph g = new Graph();
+
+        g.addEdge("A", "B");
+
+        assertEquals(2, g.getNodeCount());
+        assertEquals(1, g.getEdgeCount());
+    }
+
+    @Test
+    public void testDotOutput() {
+        Graph g = new Graph();
+
+        g.addEdge("A", "B");
+
+        String dot = g.toDotString();
+
+        assertTrue(dot.contains("A -> B"));
+    }
+}
